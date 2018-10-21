59ed683af5cdc5f497a29c0b15fd4b5e87e57c11
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-10-21 12:09:36 -0400

    preliminary controllers finished

---
+++ app/controllers/maintenances_controller.rb
@@ -0,0 +1,53 @@
+class MaintenancesController < ApplicationController
+  # Controller Code
+
+  before_action :set_maintenance, only: [:show, :update, :destroy]
+
+
+  def index
+    @maintenances = Maintenance.all
+    render json: @maintenances
+  end
+
+
+  def show
+    render json: @maintenance
+  end
+
+
+  def create
+    @maintenance = Maintenance.new(maintenance_params)
+
+    if @maintenance.save
+      render json: @maintenace, status: :created, location: @maintenance
+    else
+      render json: @maintenance.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    if @maintenance.update(maintenance_params)
+      render json: @maintenance
+    else
+      render json: @maintenance.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @maintenance.destroy
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_maintenance
+      @maintenance = Maintenance.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def maintenance_params
+      params.permit(:title, :description)
+    end
+end
